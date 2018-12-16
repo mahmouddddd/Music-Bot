9d10733861f07d4112245a4de1f7e853033eaca6
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2018-12-16 12:40:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NDk3OTA5NzgxNDI4Njk5MTQ4.Dvedrg.G99a5r68uMIwm3ZPz0Ouhldijdg';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '1';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
