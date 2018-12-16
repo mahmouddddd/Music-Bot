eae4542b1bf3f533655c2307c90fd53f6f3dddb6
mahmouddddd <45766299+mahmouddddd@users.noreply.github.com>
2018-12-16 12:47:10 +0200

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -1,4 +1,4 @@
-/*const { Client, Util } = require('discord.js');
+const { Client, Util } = require('discord.js');
 const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
